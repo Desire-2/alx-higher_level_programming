9baac8f5fe0a4dcfd0154c16d363b43a81973a4c
Desire-2 <bikorimanadesire5@gmail.com>
2023-10-03 16:57:06 +0200

    prints the ASCII alphabet, in lowercase, not followed by a new line.

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(ord('a'), ord('z') + 1):
-    if letter != ord('q') and letter != ord('e'):
-        print("{:c}".format(letter), end='' if letter != ord('z') else '\n')
+    if letter != ord('e') and letter != ord('q'):
+        print("{:c}".format(letter), end='')
