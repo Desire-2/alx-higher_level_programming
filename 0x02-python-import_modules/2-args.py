7f45ac0c034258ba7b4eedaaf4e742219c5caeed
Desire-2 <bikorimanadesire5@gmail.com>
2023-10-05 09:51:21 +0200

    program that prints the number of and the list of its arguments.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+# Get the number of arguments (excluding the script name)
+num_args = len(sys.argv) - 1
+args = sys.argv[1:]
+
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+    print("1: {}".format(args[0]))
+else:
+    print("{} arguments:".format(num_args))
+    for i, arg in enumerate(args, start=1):
+        print("{}: {}".format(i, arg))
