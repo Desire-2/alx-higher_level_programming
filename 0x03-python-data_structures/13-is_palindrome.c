1facaff05f85f00c3d3454013b9ea3988d96be68
Desire-2 <bikorimanadesire5@gmail.com>
2023-10-08 17:35:54 +0200

    Function in C that checks if a singly linked list is a palindrome.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,70 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverses linked list
+ * @head: Pointer to be pointed to the first node in the list
+ * Return: Pointer to the first node in the new list
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *nxt = NULL;
+	listint_t *prv = NULL;
+	listint_t *curr = *head;
+
+	while (curr)
+	{
+		nxt = curr->nxt;
+		curr->nxt = prv;
+		prv = curr;
+		curr = nxt;
+	}
+
+	*head = prv;
+}
+
+/**
+ * is_palindrome - Function that checks if a linked list is a palindrome
+ * @head: Double pointer of the linked list
+ * Return: 1 if it is, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slw = *head, *fst = *head, *tmp = *head, *dp = NULL;
+
+	if (*head == NULL || (*head)->nxt == NULL)
+		return (1);
+
+	while (1)
+	{
+		fst = fst->next->next;
+		if (!fst)
+		{
+			dp = slw->next;
+			break;
+		}
+		if (!fst->next)
+		{
+			dp = slw->next->next;
+			break;
+		}
+		slw = slw->next;
+	}
+
+	reverse_listint(&dp);
+
+	while (dp && tmp)
+	{
+		if (tmp->n == dp->n)
+		{
+			dp = dp->next;
+			tmp = tmp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dp)
+		return (1);
+
+	return (0);
+}
