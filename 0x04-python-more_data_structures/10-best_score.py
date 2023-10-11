ec7e37ab9a535f0c78931977e66f261993f3c311
Desire-2 <bikorimanadesire5@gmail.com>
2023-10-11 22:02:21 +0200

    Function that returns a key with the biggest integer value.

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,10 @@
 def best_score(a_dictionary):
      if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-
     retn = list(a_dictionary.keys())[0]
     bigger = a_dictionary[ret]
     for r, w in a_dictionary.items():
         if w > bigger:
-            bigger = v
+            bigger = w
             retn = r
     return (retn)
