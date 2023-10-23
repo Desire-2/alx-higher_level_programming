f652fc7e798529378eb1d14dd0341a41ab8ac55a
Desire-2 <bikorimanadesire5@gmail.com>
2023-10-23 10:06:13 -0700

    Function that divides element by element 2 lists.

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    _rslt = []
+    for r in range(list_length):
+        _div = 0
+        try:
+            _div = my_list_1[r] / my_list_2[r]
+        except (TypeError, ValueError):
+            print("wrong type")
+        except (ZeroDivisionError, ValueError):
+            print("division by 0")
+        except (IndexError, ValueError):
+            print("out of range")
+        finally:
+            rslt.append(_div)
+    return rslt
