2d5decf472a958096eb2b2e08a613488a2d40204
Desire-2 <bikorimanadesire5@gmail.com>
2023-11-06 10:38:31 -0800

    A function that adds a new attribute to an object if it’s possible:

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+"""Defines a function to check and add an attribute to an object."""
 
+def add_attribute(obj, attr_name, attr_value):
+    """
+    Checks if an attribute attr_name with value attr_value can be added to obj.
 
-def add_attribute(an_obj, an_attr, a_value):
+    Args:
+    - obj: Object to add the attribute to
+    - attr_name: Name of the attribute
+    - attr_value: Value of the attribute to add
+    """
 
-    if not hasattr(an_obj, '__slots__') and not hasattr(an_obj, '__dict__'):
-        raise TypeError("can't add new attribute")
-    if hasattr(an_obj, '__slots__') and not hasattr(an_obj, an_attr):
-        raise TypeError("can't add new attribute")
+    # Checking if an attribute can be added
+    if not hasattr(obj, '__dict__') and not hasattr(obj, '__slots__'):
+        raise TypeError("Cannot add a new attribute")
+    if hasattr(obj, '__slots__') and not hasattr(obj, attr_name):
+        raise TypeError("Cannot add a new attribute")
 
-    setattr(an_obj, an_attr, a_value)
+    # Set the attribute to the object
+    setattr(obj, attr_name, attr_value)
