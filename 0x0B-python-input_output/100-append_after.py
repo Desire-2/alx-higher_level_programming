bc90c9461d3292d4878a1dbaffc11079613e5a57
Desire-2 <bikorimanadesire5@gmail.com>
2023-11-07 11:39:41 -0800

    A function that inserts a line of text to a file, after each line containing a specific string (see example):

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,14 +4,12 @@ import sys
 from time import sleep
 import datetime
 
-def generate_random_ip():
-    return f"{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}.{random.randint(1, 255)}"
-
-def generate_log_entry():
-    return f"{generate_random_ip()} - [{datetime.datetime.now()}] \"GET /projects/260 HTTP/1.1\" {random.choice([200, 301, 400, 401, 403, 404, 405, 500])} {random.randint(1, 1024)}"
-
 for i in range(10000):
-    sleep(random.random())
-    log_entry = generate_log_entry()
-    sys.stdout.write(f"{log_entry}\n")
-    sys.stdout.flush()
+        sleep(random.random())
+        sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+                random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+                datetime.datetime.now(),
+                random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+                random.randint(1, 1024)
+                                                    ))
+        sys.stdout.flush()
