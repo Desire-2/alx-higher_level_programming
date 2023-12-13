cdaab4f9ecbdf893cdc16f27c32935340ef0a679
Desire-2 <bikorimanadesire5@gmail.com>
2023-12-13 09:09:40 -0800

    A script that lists all cities contained in the database hbtn_0d_usa.

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all Cities In The Satabase hbtn_0d_usa.
+-- Records Are Sorted in Order of Ascending cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
